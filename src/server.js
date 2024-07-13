0211c96439ee8c5d5575b111bbfdb3589cd68f72
sileeee <tldms201@naver.com>
2024-07-14 01:45:42 +0400

    pug init

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hi");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log('Listening on http://localhost:3000')
 app.listen(3000);
