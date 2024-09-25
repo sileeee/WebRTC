2f5620875c9f4ef3fb246b1d26e44ed6833e9226
sileeee <tldms201@naver.com>
2024-09-25 23:37:08 +0400

    Setting Sender

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -90,6 +90,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if(myPeerConnection){
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+    .getSenders()
+    .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 function addMessage(message) {
@@ -203,7 +210,7 @@ function makeConnection(){
   console.log(myStream.getTracks());
   myStream
     .getTracks()
-    .forEach(track => myPeerConnection.addTrack(track, myStream));
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
 function handleIce(data){
